083407c93d68459cba0f5bc768b31f30e043baa7
ChrisferDev <ferchris82@hotmail.com>
2024-04-07 14:33:17 -0500

    Update 1.0

---
+++ src/Menor.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Menor {
+
+    /*
+     * Pide 2 números por consola e indica cual es el menor de los 2 numeros .
+     * En caso de que sean iguales, también lo debes indicar.
+     */
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Dame le primer número");
+        int num1 = sc.nextInt();
+
+        System.out.println("Dame el segundo número");
+        int num2 = sc.nextInt();
+
+        if (num1 < num2) {
+            System.out.println("El número " + num1 + " es menor al número " + num2);
+        } else if (num1 == num2) {
+            System.out.println("Es el mismo número");
+        } else {
+            System.out.println("El número " + num2 + " es menor al número " + num1);
+        }
+
+    }
+
+}
