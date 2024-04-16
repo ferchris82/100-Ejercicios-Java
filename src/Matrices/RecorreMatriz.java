c01cab47826711580ab33df1a2dcc849ed27cb96
ChrisferDev <ferchris82@hotmail.com>
2024-04-16 14:32:35 -0500

    Version 1.4

---
+++ src/Matrices/RecorreMatriz.java
@@ -0,0 +1,36 @@
+package Matrices;
+
+import java.util.Scanner;
+
+public class RecorreMatriz {
+    /* 
+     * Crea una matriz de números de 3x3, pidiendo cada número.
+     * Al final recorre la matriz y muestrala.
+     */
+
+     public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        int [][] matriz = new int [3][3];
+        /* 
+         *  0 0 0
+         *  0 0 0
+         *  0 0 0
+         */
+        // rellenar
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                System.out.println("Escribe un número en la posición i: " + i + "");
+                matriz[i][j] = sc.nextInt();
+            }
+        }
+        // mostrar
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[0].length; j++) {
+                System.out.print(matriz[i][j]+" ");
+            }
+            System.out.println();
+        }
+     }
+}
