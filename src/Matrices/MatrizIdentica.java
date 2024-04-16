a9e24216813f1f6ab65d731ae0f1c1ba08196b13
ChrisferDev <ferchris82@hotmail.com>
2024-04-16 16:05:33 -0500

    Versión 1.4

---
+++ src/Matrices/MatrizIdentica.java
@@ -0,0 +1,35 @@
+package Matrices;
+
+public class MatrizIdentica {
+    /*
+     * Crea una matriz de números de 3x3 tu mismoo.
+     * Crea una función que pasandole la matriz y la fila a revisar,
+     * debe indicar si los valores de esa fila son identicos.
+     */
+
+    public static void main(String[] args) {
+
+        int[][] matriz = {
+                { 1, 2, 3 },
+                { 4, 4, 4 },
+                { 7, 7, 7 }
+        };
+
+        System.out.println(filaIdenticaMatriz(matriz, 2));
+    }
+
+    public static boolean filaIdenticaMatriz(int[][] matriz, int fila) {
+        if (fila >= 0 && fila < matriz.length) {
+            int primerValor = matriz[fila][0];
+            for (int j = 1; j < matriz[0].length; j++) {
+                if (primerValor != matriz[fila][j]) {
+                    return false;
+                }
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
